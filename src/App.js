787042914113b0f2f0672c3ca7891152de6ab768
McSho <McSho@github.com>
2024-10-03 05:34:14 -0400

    Created a React component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import necessary modules from React library
-import React, { useEffect } from 'react';
+import React from 'react';
 
 // Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,22 +7,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
 
+// Import the LandingPage component
+import LandingPage from './Components/Landing_Page/Landing_Page'; // Ensure the correct import path
+
 // Function component for the main App
 function App() {
 
   // Render the main App component
   return (
     <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar/>
 
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-          </Routes>
-        </BrowserRouter>
+        {/* Set up the Routes for different pages */}
+        <Routes>
+          {/* Define the route for the LandingPage component */}
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
